c4da9998e8eefbbe2c0e18a372f3aef0a151faf6
Autthawigorn Yortpiboot <twiggyramirez8@gmail.com>
2026-05-25 16:25:15 +0700

    .

---
+++ Sources/HelloVapor/Models/Movies.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  HelloVapor
+//
+//  Created by Art's Mac M1 on 5/25/26.
+//
+
+import Foundation
+import Vapor
+
+struct Movies: Content {
+    let title: String
+}
--- Sources/HelloVapor/routes.swift
+++ Sources/HelloVapor/routes.swift
@@ -5,20 +5,41 @@ func routes(_ app: Application) throws {
     app.get { req async in
         "It works!"
     }
-
+    
     //http://127.0.0.1:8080/hello
     app.get("hello") { req async -> String in
         "Hello, world!"
     }
     
-    //http://127.0.0.1:8080/movies
-    app.get("movies") { req async -> String in
-        "Movies"
+    
+    app.get("customers", ":customerId") { req async throws -> String in
+        guard let customerId = req.parameters.get("customerId", as: Int.self) else {
+            throw Abort(.badRequest)
+        }
+        return "\(customerId)"
     }
     
+    //http://127.0.0.1:8080/movies
+    app.get("movies") { req async -> [Movies] in
+            [Movies(title: "Dummy")]
+        }
+    
+    ///route parameters/ dynamic parameter
     //movies/action
     //movies/dummy
-    app.get("movies", ":genre") { req async -> String in
-        "Action Movies"
+    app.get("movies", ":genre") { req async throws -> String in
+        guard let genre = req.parameters.get("genre") else {
+            throw Abort(.badRequest)
+        }
+        return "Movies for \(genre)"
+    }
+    
+    app.get("movies", ":genre", ":year") { req async throws -> String in
+        guard let genre = req.parameters.get("genre"),
+              let year = req.parameters.get("year")
+        else {
+            throw Abort(.badRequest)
+        }
+        return "All movies of \(genre) from \(year)"
     }
 }
