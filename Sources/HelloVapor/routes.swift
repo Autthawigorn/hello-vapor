d6bf8cd9835c6070eb0f5a00ee00085dc8cfb810
Autthawigorn Yortpiboot <twiggyramirez8@gmail.com>
2026-05-25 12:43:47 +0700

    01

--- Sources/HelloVapor/routes.swift
+++ Sources/HelloVapor/routes.swift
@@ -1,11 +1,24 @@
 import Vapor
 
 func routes(_ app: Application) throws {
+    //http://127.0.0.1:8080
     app.get { req async in
         "It works!"
     }
 
+    //http://127.0.0.1:8080/hello
     app.get("hello") { req async -> String in
         "Hello, world!"
+    }
+    
+    //http://127.0.0.1:8080/movies
+    app.get("movies") { req async -> String in
+        "Movies"
+    }
+    
+    //movies/action
+    //movies/dummy
+    app.get("movies", ":genre") { req async -> String in
+        "Action Movies"
     }
 }
