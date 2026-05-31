1dc691461908e4f0ff5558296fab50125fe40640
Canned Cat <autthawigorn@gmail.com>
2026-05-31 17:54:39 +0700

    done

--- Sources/HelloVapor/Models/HotelQuery.swift
+++ Sources/HelloVapor/Models/HotelQuery.swift
@@ -5,10 +5,10 @@
 //  Created by Art Mac M5 on 26/5/2569 BE.
 //
 
-import Foundation
-import Vapor
-
-struct HotelQuery: Content {
-    let sort: String?
-    let search: String?
-}
+//import Foundation
+//import Vapor
+//
+//struct HotelQuery: Content {
+//    let sort: String?
+//    let search: String?
+//}
--- Sources/HelloVapor/routes.swift
+++ Sources/HelloVapor/routes.swift
@@ -8,6 +8,59 @@ func routes(_ app: Application) throws {
         try await movie.save(on: req.db)
         return movie
     }
+    
+    // get all movies
+    app.get("movies") { req async throws in
+      try await Movies.query(on: req.db)
+            .all()
+    }
+    
+    // get movie by id
+    app.get("movies", ":id" ) { req async throws in
+        guard let movie = try await Movies.find(req.parameters.get("id"), on: req.db) else {
+            throw Abort(.notFound)
+        }
+        return movie
+        
+    }
+    
+    // delete a movie
+    app.delete("movies", ":id") { req async throws in
+        guard let movie = try await Movies.find(req.parameters.get("id"), on: req.db) else {
+            throw Abort(.badRequest)
+        }
+        
+        try await movie.delete(on: req.db)
+        return movie
+    }
+    
+    // update a movie
+//    app.put("movies") { req async throws in
+//       let movie = try req.content.decode(Movies.self)
+//        
+//        guard let movieToUpdate = try await Movies.find(movie.id, on: req.db) else {
+//            throw Abort(.notFound)
+//        }
+//        
+//        movieToUpdate.title = movie.title
+//        
+//        try await movieToUpdate.update(on: req.db)
+//        return movieToUpdate
+//    }
+    
+    app.put("movies") { req async throws in
+           let movie = try req.content.decode(Movies.self)
+            
+        guard let movieToUpdate = try await Movies.find(movie.id, on: req.db) else {
+                throw Abort(.notFound)
+            }
+            
+            movieToUpdate.title = movie.title
+            
+            try await movieToUpdate.update(on: req.db)
+            return movieToUpdate
+        }
+
  
 //    app.middleware.use(LogMiddleware())
 //    app.middleware.use(AuthenticationMiddleWare())
